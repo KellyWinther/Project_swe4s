0cccd4c8023a7a52168576452867f398a217df41
Autumn Stephens <aust8150@colorado.edu>
2025-10-31 13:30:47 -0600

    Adjusted example scripts to work with updated, generalized functions

--- src/example_call.py
+++ src/example_call.py
@@ -1,13 +1,19 @@
 from loading_utils import load_spike_data, match_times
+from analysis_utils import make_correlation_dictionary, visualize_correlation_dictionary
 
 spike_df = load_spike_data()
 
-df = match_times(spike_df, progress=True)
+# Finds which spikes happened during a SWR
+df = match_times(
+    spike_df,
+    "../src/SWRs_7744_partner_intro.csv",
+    filter_event_data = {"KSLabel": ["good"]},
+    keep_event_columns = ["Time", "Cluster ID"],
+    progress=True,
+)
 
 print("First ten rows of extracted data...")
 print(df[:10])
-
-from analysis_utils import *
 
 # Collects correlation data for neuron clusters
 corr_matrix = make_correlation_dictionary(df, normalize=False)
