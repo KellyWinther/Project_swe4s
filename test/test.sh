ded79b5b71c87018aa9f3845ba4f21c871a3e7fb
Audrey <audrey@grantski.com>
2025-11-03 12:22:45 -0700

    remove test from sh

--- test/test.sh
+++ test/test.sh
@@ -12,16 +12,3 @@ python="python3"
 script="src/loading_utils.py"
 data="test/test_data/subset_agrofood_co2_emission.csv"
 
-run test_no_calculate $python $script \
-    --country "Albania" \
-    --country_column 0 \
-    --fires_column 2 \
-    --file_name $data 
-
-    assert_in_stdout "Found 5 entries for 'Albania'"
-    assert_in_stdout 5
-    assert_in_stdout 5
-    assert_in_stdout 5
-    assert_in_stdout 5
-    assert_in_stdout 5
-    assert_exit_code 0
